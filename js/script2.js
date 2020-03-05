d02f939cc3ba9474caf2b0ec7acc22956b7735e4
PascualHache <hector.pascual.diaz@gmail.com>
2020-03-05 10:54:33 +0100

    Finiquitado

--- js/script2.js
+++ js/script2.js
@@ -1,4 +1,5 @@
 var mapid = L.map('mapid').setView([41.386956, 2.170003], 16);
+var layerGroup = L.layerGroup().addTo(mapid);
 
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
@@ -7,22 +8,16 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 L.control.scale().addTo(mapid);
 
-var latitud = 41.386889;
-var longitud = 2.166028;
-
-L.marker([latitud, longitud], { draggable: true }).addTo(mapid);
-
-var popup = L.popup();
-
 function onMapClick(e) {
-    console.log(e.latlng);
-    if (true) {
-        
+    // console.log(e.latlng);  
+    layerGroup.clearLayers();
+    mapid.setView(e.latlng, 42);
+    L.marker([e.latlng["lat"], e.latlng["lng"]], { draggable: true }).addTo(layerGroup);
         popup
-            .setLatLng(e.latlng)
+            .setLatLng({"lat":e.latlng["lat"]+0.0001, "lng":e.latlng["lng"]})
             .setContent("Mis coordenadas son:<br><strong>Lat:" + e.latlng["lat"]+"  Len:"+e.latlng["lng"])
             .openOn(mapid);
-    }
+            
 }
-
+var popup = L.popup();
 mapid.on('click', onMapClick);
